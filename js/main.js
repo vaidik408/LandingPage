ffad55103a0703dc4c87e7ab88b132d048e86d3a
Vaidik Modi <modivaidik408@gmail.com>
2024-01-08 16:47:47 +0530

    completed

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-const menuBtn = document.querySelector('#mobile-menu-btn');
-const menuBars = menuBtn.querySelector('span');
-
-const nav = document.querySelector('#main-nav');
-
-let showMenu = false;
-
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    
-    if (!showMenu) {
-        nav.classList.add('open');
-        menuBtn.classList.add('open');
-        menuBars.classList.add('open');
-    } else {
-        nav.classList.remove('open');
-        menuBtn.classList.remove('open');
-        menuBars.classList.remove('open');
-    }
-
-    showMenu = !showMenu;
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
 }
